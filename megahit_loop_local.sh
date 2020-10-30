cd7d652c6f40ef842f097118a075c71dac0fc402
H.L. Freund <hfreu002@ucr.edu>
2020-10-30 09:59:43 -0700

    updated annotations

--- megahit_loop_local.sh
+++ megahit_loop_local.sh
@@ -9,17 +9,17 @@ do
     SAMPLE=$(echo ${i} | sed "s/_L001_R1_\CLEANEST\.fastq//")
     echo ${SAMPLE}_L001_R1_CLEANEST.fastq ${SAMPLE}_L001_R2_CLEANEST.fastq
     
-    /opt/anaconda3/bin/megahit -1 ${SAMPLE}_L001_R1_CLEANEST.fastq -2 ${SAMPLE}_L001_R2_CLEANEST.fastq -o ./megahit_${SAMPLE}_assembly_${today}
+     /opt/anaconda3/bin/megahit -1 ${SAMPLE}_L001_R1_CLEANEST.fastq -2 ${SAMPLE}_L001_R2_CLEANEST.fastq -r ${SAMPLE}_L001_merged.fastq -t 12 -o ./megahit_${SAMPLE}_assembly_${today}
     
 done
 
-# Megahit assembly tips
+# MEGAHIT assembly tips
 # for ultra complex metagenomics data such as soil, a larger kmin, say 27, is recommended to reduce the complexity of the de Bruijn graph.
 ### Quality trimming is also recommended
 # --min-count 2 ----> (kmin+1)-mer with multiplicity lower than d (default 2, specified by --min-count option) will be discarded;
 ### recommend using the default value 2 for metagenomics assembly
-#
 
-# megahit -1 EA_Pool-POW_1-1a_S28_L001_R1_CLEANEST.fastq -2 EA_Pool-POW_1-1a_S28_L001_R2_CLEANEST.fastq -r ${SAMPLE}_L001_merged.fastq -o ./megahit_assembly
+## MORE MEGAHIT TIPS: https://sites.google.com/site/wiki4metagenomics/tools/assembly/megahit
 
-#/opt/anaconda3/bin/megahit ---- for running on my computer
+
+#/opt/anaconda3/bin/megahit ---- path for megahit program; for running on my computer
