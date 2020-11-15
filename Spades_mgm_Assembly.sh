1518230f46c56a395ac7347876f165b499a9e881
H.L. Freund <hfreu002@ucr.edu>
2020-11-15 15:52:03 -0800

    updated script to change and copy scaffolds.fasta file

--- Spades_mgm_Assembly.sh
+++ Spades_mgm_Assembly.sh
@@ -14,6 +14,8 @@
 
 cd /bigdata/aronsonlab/shared/HannahFreund/HannahFTemp/
 
+Path="/bigdata/aronsonlab/shared/HannahFreund/HannahFTemp/"
+
 today=$(date "+%m.%d.%y") # date is the command to get today's date, and the "+%m_%d_%y" will print it in month_day_year format
 
 module load SPAdes/3.14.1
@@ -25,6 +27,9 @@ do
     
     metaspades.py -1 ${SAMPLE}_L001_R1_normalized_error_corrected.fastq.gz -2 ${SAMPLE}_L001_R2_normalized_error_corrected.fastq.gz --merged ${SAMPLE}_normalized_merged_error_corrected.fastq.gz -s ${SAMPLE}_L001_R_unpaired_error_corrected.fastq.gz -o ${SAMPLE}_assembly_SPades_${today} --meta -t 4 --only-assembler
     
+    mv ${Path}/${SAMPLE}_assembly_SPades_${today}/scaffolds.fasta ${Path}/${SAMPLE}_assembly_SPades_${today}/${SAMPLE}_scaffolds.fasta
+    
+    cp ${Path}/${SAMPLE}_assembly_SPades_${today}/${SAMPLE}_scaffolds.fasta ${Path}/
     #echo -e "Finished \aassembling ${SAMPLE}_L001_R1 ${SAMPLE}_L001_R2"
 
 done
